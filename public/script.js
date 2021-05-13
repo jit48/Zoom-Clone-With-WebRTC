25979d0cf0f2274baa6721fb0f3008580559fc2c
jit48 <ghoshjitkumar89@gmail.com>
2021-05-13 16:07:39 +0530

    New

--- public/script.js
+++ public/script.js
@@ -5,12 +5,10 @@ const videoGrid = document.getElementById('video-grid')
 //   port: '3001'
 // })
 
-const myPeer = new Peer({
-  //key: 'peerjs',
-  host: 'mysterious-cliffs-71245.herokuapp.com',
-  port: 443,
+var myPeer = new Peer(undefined, {
   path: '/peerjs',
-  //secure: true,
+  host: '/',
+  port: '443'
 });
 
 // const myPeer = new peers();
