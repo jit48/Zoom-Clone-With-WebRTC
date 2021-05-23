9ca61e6a50a7c65d9b78f2a2353319630859bd17
jit48 <ghoshjitkumar89@gmail.com>
2021-05-23 22:00:09 +0530

    Fixed bugs

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-// const myPeer = new Peer(undefined, {
-//   host: '/',
-//   port: '3001'
-// })
+
 
 myPeer = new Peer(undefined, {})
 let ownVideo;
